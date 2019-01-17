5d69523dbad0567020666593be70e32ecf440ab8
pazadeh <38374357+pazadeh@users.noreply.github.com>
2019-01-17 16:31:06 +0100

    Update BAAM_train.py

--- BAAM_train.py
+++ BAAM_train.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2017, Oren Kraus All rights reserved.
+# Copyright (c) 2017, Pouyan Asgharzadeh All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or without modification,
 # are permitted provided that the following conditions are met:
