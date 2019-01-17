d76cfc991d46a63f6790258b2f2fdbeb6476a0f2
pazadeh <38374357+pazadeh@users.noreply.github.com>
2019-01-17 15:46:05 +0100

    Add files via upload

---
+++ Size_Homogenizer_train_GPU.m
@@ -0,0 +1,87 @@
+%% This code homogenizes the size of the images. 
+% This happens based on the give (or found) maximal sizes in X and Y and
+% minimal size in the Z direction from the top
+
+
+
+clear 
+clc
+cd('/media/asgharpn/daten2017-03/Bone_Machine_learning/Learning_dataset/Original_images')
+files = dir('*.mat');
+image_sizes = [];
+addpath('/media/asgharpn/daten2017-03/Protein_Machine_learning/Functions/');
+
+
+original_images_path  = '/media/asgharpn/daten2017-03/Bone_Machine_learning/Learning_dataset/Original_images';
+resampled_images_path = '/media/asgharpn/daten2017-03/Bone_Machine_learning/Learning_dataset/resampled_borders_test_01';
+% 
+% for i=1:length(files)
+%     img = load(files(i).name);
+%     image = struct2cell(img(1));
+%     image = image{1,1};
+%     image_sizes = [image_sizes;size(image)];
+%     
+% end
+
+min_z_dim =161+1;
+
+max_x_dim =602;
+max_y_dim =733;
+
+for i=1:length(files)
+    % Reading the image
+    cd(original_images_path)
+    img = load(files(i).name);
+    image_c = struct2cell(img(1));
+    image_c = image_c{1,1};
+    image = gpuArray(image_c);    
+    image = double(image);
+    
+    % Cropping th Z direction
+    image(:,:,(min_z_dim:size(image,3))) = [];
+
+    % Finding the stripes of pixels in left and right of the imahe
+    x_strip_left  = image(:,1,:);
+    x_strip_right = image(:,size(image,2),:);
+    % Adding the strips to the image
+    for j=1:(fix((max_y_dim-size(image,2))/2))
+        image = cat(2,x_strip_left,image);
+        image = cat(2,image,x_strip_right);
+    end
+    
+    if size(image,2)<max_y_dim
+       image = cat(2,image,x_strip_right); 
+    end
+     if size(image,2)>max_y_dim
+       image(1,:,:)= []; 
+     end
+    
+    % Finding the stripes of pixels on top and bottom of the image
+    y_strip_top   = image(1,:,:);
+    y_strip_bot   = image(size(image,1),:,:);
+    % Adding the strips to the image
+    for j=1:(fix((max_x_dim-size(image,1))/2))
+        image = cat(1,y_strip_top,image);
+        image = cat(1,image,y_strip_bot);
+    end
+    if size(image,1)<max_x_dim
+       image = cat(1,image,y_strip_bot); 
+    end
+     if size(image,1)>max_x_dim
+       image(:,1,:)= []; 
+    end
+
+    
+    cd(resampled_images_path)
+    % Correcting the name of the image
+    file_name = strcat(erase(files(i).name,'.mat'),'.mat');
+    % Gathering the image and bringing it to int16 format
+    image =int16(image);
+    image_c = gather(image);
+    % Saving the image
+    save(file_name,'image_c','-mat');
+    i/length(files)
+
+    
+end
+
