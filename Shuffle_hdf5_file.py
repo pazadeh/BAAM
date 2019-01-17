676c29d12e59ace8f75a2e24d420115866970fe8
pazadeh <38374357+pazadeh@users.noreply.github.com>
2019-01-17 16:08:36 +0100

    Update Shuffle_hdf5_file.py

--- Shuffle_hdf5_file.py
+++ Shuffle_hdf5_file.py
@@ -1,10 +1,6 @@
-# -*- coding: utf-8 -*-
 """
-Created on Wed Apr 11 18:13:40 2018
-
-@author: asgharpn
+shuffleing the augmentated data to create train, validation and test data set
 """
-
 
 import numpy as np
 import h5py
@@ -13,7 +9,7 @@ from random import shuffle
 
 
 
-images_path        = '/media/asgharpn/daten2017-03/Bone_Machine_learning/Learning_dataset/projected_augmented_not_squared_yz_test_01_without_2mice_09/'
+images_path        = 'augmentated dataset location'
 ip_augmented_path  = images_path+'dataset_projected_augmented.hdf5'
 f_or               = h5py.File(ip_augmented_path,'r')
 keys = f_or.keys()
@@ -27,22 +23,16 @@ image_size_y= 733
 image_size_z= 161
 # read addresses and labels from the 'train' folder with a hot vector
 image_order = np.arange(dset_images.shape[0])
-#labels = [[1,0,0,0] if 'day0' in addr else 
-#         [0,1,0,0] if 'day5'  in addr else  
-#         [0,0,1,0] if 'day10' in addr else  
-#         [0,0,0,1] if 'day15' in addr else 'bad naming of images'
-#         for addr in addrs]
-# to shuffle data
 if shuffle_data:
     c = list(zip(image_order))
     shuffle(c)
     image_order = zip(*c)
 
 image_order =np.asarray(image_order)
-# Divide the hata into 90% train, 10% validation, and 10% test 
+# Divide the hata into 80% train, 10% validation, and 10% test 
 train_order = image_order[0][0:int(0.80*image_order.shape[1])]
-valid_order = image_order[0][int(0.80*image_order.shape[1]):]
-test_order  = image_order[0][int(0.80*image_order.shape[1]):]
+valid_order = image_order[0][int(0.80*image_order.shape[1]):int(0.90*image_order.shape[1])]
+test_order  = image_order[0][int(0.90*image_order.shape[1]):]
 
 # check the order of data and chose proper data shape to save images
 train_shape = (len(train_order), image_size_y*image_size_z)
@@ -50,7 +40,7 @@ val_shape   = (len(valid_order), image_size_y*image_size_z)
 test_shape  = (len(test_order),  image_size_y*image_size_z)
 
 #Setting the path to shuffled and divided files
-shuffled_images_path ='/media/asgharpn/daten2017-03/Bone_Machine_learning/Learning_dataset/projected_augmented_not_squared_yz_test_01_without_2mice_10/'
+shuffled_images_path ='location of train, test, validation datasets'
 bone_cr10p_top_train_set_path = shuffled_images_path+'bone_projected_train_set.hdf5'  
 bone_cr10p_top_test_set_path  = shuffled_images_path+'bone_projected_test_set.hdf5'  
 bone_cr10p_top_valid_set_path = shuffled_images_path+'bone_projected_valid_set.hdf5'  
@@ -92,18 +82,7 @@ for i in valid_order:
     pbar.update(1)
 counter =0 
 
-#for i in test_order:
-#    img   = dset_images[i]
-#    label = dset_indexes[i]
-#    # add any image pre-processing here
-#    img = img.flatten()
-#    bone_cr10p_top_test_set["data1"][counter, ...] = img[None]
-#    bone_cr10p_top_test_set["Index1"][counter, ...] = label
-#    counter = counter+1
-#    pbar.update(1)
-#    
 
-#plt.imshow(bone_cr10p_top_test_set["data1"][counter-1])
 f_or.close()
 bone_cr10p_top_train_set.close()
 bone_cr10p_top_valid_set.close()
